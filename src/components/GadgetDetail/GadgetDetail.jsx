5368eb45107588ffc2a9feb63aab4cab26ab9767
Sudip Kumar <keya24gadu2020@gmail.com>
2024-11-05 21:43:43 +0600

    fixed cards

--- src/components/GadgetDetail/GadgetDetail.jsx
+++ src/components/GadgetDetail/GadgetDetail.jsx
@@ -1,5 +1,7 @@
 import { useLoaderData, useParams } from "react-router-dom"
 import { GiSelfLove } from "react-icons/gi";
+import { TiShoppingCart } from "react-icons/ti";
+
 
 const GadgetDetail = () => {
     const { product_id } = useParams();
@@ -7,26 +9,27 @@ const GadgetDetail = () => {
     const gadget = data.find(gadget => gadget.product_id === parseInt(product_id))
     const { product_title, product_image, category, price, description, Specification, availability, rating } = gadget;
     return (
-        <div className="static">
+        <div className="static ">
             <div className="text-center bg-[#9538E2] pb-32">
-                <h1 className="text-2xl font-bold">Product Details</h1>
-                <p>Explore the latest gadgets that will take your experience to the next level. From smart devices to the coolest accessories, we have it all!</p>
+                <h1 className="text-2xl font-bold text-[#FFFFFF]">Product Details</h1>
+                <p className="text-[#FFFFFF]">Explore the latest gadgets that will take your experience to the next level. From smart devices to the coolest accessories, we have it all!</p>
             </div>
-            <div className="hero bg-[#FFFFFF] h-96 min-w-full  border-2">
+            <div className="hero bg-[#FFFFFF] h-96 w-8/12 flex justify-center mx-auto  border-2 static -mt-20 rounded-2xl mb-10">
                 <div className="hero-content flex-col lg:flex-row">
                     <img
                         src={product_image}
-                        className="max-w-sm rounded-lg shadow-2xl" />
+                        className="max-w-sm h-80 rounded-lg shadow-2xl" />
                     <div>
-                        <h1 className="text-5xl font-bold">{product_title}</h1>
-                        <p>{price}</p>
-                        <button className="btn btn-outline btn-success rounded-full">Success</button>
+                        <h1 className="text-5xl font-bold mb-1">{product_title}</h1>
+                        <p><span className="font-bold">Price: </span>${price}</p>
+                        <button className="btn btn-outline btn-success rounded-full bg-[#309C081A] mt-1">In Stock</button>
                         <p>{description}</p>
-                        <p>{Specification}</p>
-                        <p>{rating}</p>
-                        <button className="btn btn-active btn-secondary rounded-full">Secondary</button>
+                        <p className="font-bold">Specification:</p>
+                        <p> <li>{Specification}</li></p>
+                        <p><span className="font-bold">Rating ⭐: </span>{rating}</p>
+                        <button className="btn btn-active text-[#FFFFFF] rounded-full bg-[#9538E2]">Add To Card <TiShoppingCart /></button>
                         <button className="btn btn-circle btn-outline bg-[#FFFFFF]">
-                            <GiSelfLove className="w-8 h-8" />
+                            <GiSelfLove className="w-4 h-4" />
                         </button>
                     </div>
                 </div>
